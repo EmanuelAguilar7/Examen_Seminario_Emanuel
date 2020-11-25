032f49e19411d54d6f5677a6c1758d71b584e388
Emanuel <emanuelaguilar68@yahoo.com>
2020-11-24 18:38:18 -0600

    Ruta para obtener una orden

--- Examen/routes/api/productos.js
+++ Examen/routes/api/productos.js
@@ -22,5 +22,15 @@ router.get('/all', (req, res)=>{
 res.status(200).json(productosArray);
 } );
 
+router.get('/one/:id', (req, res)=>{
+    let { id } = req.params;
+    id = Number(id);
+    let products = productosArray.find((o, i)=>{
+    return o.id === id;
+    })
+    res.status(200).json(products);
+
+});
+
 
 module.exports = router;
